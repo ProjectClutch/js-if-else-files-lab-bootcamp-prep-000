fb3c0532b3ec3ec57e618d0e9d988830d5b093da
Project Clutch <usaparagraff@gmail.com>
2017-10-07 03:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan") {
+  
+} else {
+  console.log("Name expected to be Susan");
+}
+
 
 // Don't worry about this
 module.exports = { name, height
